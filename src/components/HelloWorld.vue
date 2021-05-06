80279d49ff9db98e3d49ca201b4d5bc57add7a62
dropInInt <hzy452347705@163.com>
2021-05-06 21:11:30 +0800

    新建项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <p>{{ data.counter }}</p>
+</template>
+
+<script>
+import { reactive, computed } from 'vue'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  setup() {
+    const data = reactive({
+      counter: '111'
+    })
+    return { data }
+  }
+}
+</script>
---
+++ src/views/Index.vue
@@ -0,0 +1,12 @@
+<template>
+ <div class="index" @click="go()">
+  首页
+ </div>
+</template>
+<script>
+export default {
+ name: 'Index',
+ setup() {
+ }
+}
+</script>
